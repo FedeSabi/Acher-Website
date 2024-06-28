2c7c32fb01e0a0b8b76d208109a8e5f1a443b113
Federico Sabiche <fsabiche>
2024-06-28 16:20:18 -0300

    creacion de footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-300 h-[300px] dark:bg-gray-800 mt-20 pt-4 pb-8 xl:pt-8">
+        <div className="max-w-screen-lg px-4 mx-auto text-gray-400 xl:max-w-screen-xl sm:px-6 md:px-8 dark:text-gray-300">
+        <div className="flex flex-col items-center bg-gray-300">
+        <button className="w-[442px] h-[120px] -mt-24  mb-6 bg-gray-300 text-6xl rounded-lg text-black">Contacto</button>
+        </div>
+            
+        </div>
+    </footer>
+  )
+}
+export default Footer
