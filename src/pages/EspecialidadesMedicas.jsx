1735da2e2af118df7263735fa7134e680f1a07f5
Federico Sabiche <fsabiche>
2024-07-16 19:08:37 -0300

    responsive especialidades medicas

--- src/pages/EspecialidadesMedicas.jsx
+++ src/pages/EspecialidadesMedicas.jsx
@@ -2,13 +2,13 @@ const EspecialidadesMedicas = () => {
   return (
     <div>
       <div className="flex flex-row justify-center items-center w-full h-[171px] mt-[120px] bg-[#d9f0f9] font-lato">
-        <h1 className="text-6xl text-[#2987BC]">Especialidades Médicas</h1>
+        <h1 className="sm:text-6xl text-3xl text-[#2987BC]">Especialidades Médicas</h1>
       </div>
       {/*primer fila */}
       <div className="sm:flex flex-row justify-around mt-14 gap-14 m-12">
-        <div className="w-1/3 flex flex-col gap-10">
-          <h1 className="text-4xl text-[#2987BC]">Cardiología</h1>
-          <ul className="text-xl list-disc list-inside text-[#505050] marker-red">
+        <div className="w-full sm:w-2/3 md:w-1/3 flex flex-col gap-10 mb-10">
+          <h1 className="sm:text-4xl text-3xl text-[#2987BC]">Cardiología</h1>
+          <ul className="sm:text-xl text-md list-disc list-inside text-[#505050] marker-red">
             <li>AGUJAS DE BIOPSIA</li>
             <li>INTRODUCTORES VASCULARES</li>
             <li>GUÍAS ESTANDARES / CURVAS</li>
@@ -20,9 +20,9 @@ const EspecialidadesMedicas = () => {
             <li>STENTS</li>
           </ul>
         </div>
-        <div className="w-1/3 flex flex-col gap-10">
-          <h1 className="text-4xl text-[#2987BC]">Gastroenterología</h1>
-          <ul className="text-xl list-disc list-inside text-[#505050] marker-red">
+        <div className="w-full sm:w-2/3 md:w-1/3 flex flex-col gap-10">
+          <h1 className="sm:text-4xl text-3xl text-[#2987BC]">Gastroenterología</h1>
+          <ul className="sm:text-xl text-md list-disc list-inside text-[#505050] marker-red">
             <li>PINZAS DE BIOPSIA</li>
             <li>GUÍAS</li>
             <li>PAPILOTOMOS</li>
@@ -40,9 +40,9 @@ const EspecialidadesMedicas = () => {
             <li>EQUIPOS LÁSER PARA LITOTRICIA</li>
           </ul>
         </div>
-        <div className="w-1/3 flex flex-col gap-10">
-          <h1 className="text-4xl text-[#2987BC]">Intervencionismo</h1>
-          <ul className="text-xl list-disc list-inside text-[#505050] marker-red">
+        <div className="w-full sm:w-2/3 md:w-1/3 flex flex-col gap-10">
+          <h1 className="sm:text-4xl text-3xl sm:mt-0 mt-10 text-[#2987BC]">Intervencionismo</h1>
+          <ul className="sm:text-xl text:md list-disc list-inside text-[#505050] marker-red">
             <li>DRENAJES</li>
             <li>TRANSYUGULAR</li>
             <li>AGUJAS DE BIOPSIA</li>
@@ -55,9 +55,9 @@ const EspecialidadesMedicas = () => {
       </div>
       {/*segunda fila */}
       <div className="sm:flex flex-row justify-around mt-14 gap-14 m-12">
-        <div className="w-1/3 flex flex-col gap-10">
-          <h1 className="text-4xl text-[#2987BC]">Urología</h1>
-          <ul className="text-xl list-disc list-inside text-[#505050] marker-red">
+        <div className="w-full sm:w-2/3 md:w-1/3 flex flex-col gap-10">
+          <h1 className="sm:text-4xl text-3xl text-[#2987BC]">Urología</h1>
+          <ul className="sm:text-xl text-md list-disc list-inside text-[#505050] marker-red">
             <li>CATÉTERES DOBLE J / URETERALES</li>
             <li>EXTRACTORES</li>
             <li>BALONES / DILATADORES</li>
@@ -71,9 +71,9 @@ const EspecialidadesMedicas = () => {
             <li>EQUIPOS LÁSER PARA LITOTRICIA</li>
           </ul>
         </div>
-        <div className="w-1/3 flex flex-col gap-10">
-          <h1 className="text-4xl text-[#2987BC]">Cuidados Críticos</h1>
-          <ul className="text-xl list-disc list-inside text-[#505050] marker-red">
+        <div className="w-full sm:w-2/3 md:w-1/3  flex flex-col gap-10">
+          <h1 className="sm:text-4xl text-3xl sm:mt-0 mt-10 text-[#2987BC]">Cuidados Críticos</h1>
+          <ul className="sm:text-xl text-md list-disc list-inside text-[#505050] marker-red">
             <li>MONITOREO</li>
             <li>DIALISIS</li>
             <li>LAPAROSCOPIA</li>
@@ -81,14 +81,14 @@ const EspecialidadesMedicas = () => {
             <li>EMERGENCIA RESPIRATORIA</li>
           </ul>
         </div>
-        <div className="w-1/3 flex flex-col gap-10">
-          <h1 className="text-4xl text-[#2987BC]">
+        <div className="w-full sm:w-2/3 md:w-1/3 flex flex-col gap-10">
+          <h1 className="sm:text-4xl text-3xl sm:mt-0 mt-10 text-[#2987BC]">
             Ginecología - Reproducción{" "}
           </h1>
-          <ul className="text-xl list-disc list-inside text-[#505050] marker-red">
+          <ul className="sm:text-xl text-md list-disc list-inside text-[#505050] marker-red">
             <li>REPRODUCCIÓN</li>
             <li>OBSTETRICIA</li>
-            <li>INCUBADORAS</li>
+            <li className="sm:mb-0 mb-20">INCUBADORAS</li>
           </ul>
         </div>
       </div>
