2febdffaa801f7a903a8156c2d07a68aa28c5956
Federico Sabiche <fsabiche>
2024-08-07 19:54:30 -0300

    otra modificacion

--- src/components/ButtonsHome2.jsx
+++ src/components/ButtonsHome2.jsx
@@ -157,9 +157,9 @@ const ButtonsHome2 = () => {
             </span>
           </button>
           <div
-            className={`transition-all duration-500 ease-in-out w-screen ${
+            className={`transition-all duration-500 ease-in-out w-screencommi ${
               openSection ? "max-h-[1200px]" : "max-h-0"
-            } overflow-hidden absolute left-0 right-0 bg-[#e7e5e5] sm:mt-[150px] mt-80 sm:-ml-[0px] md:-ml-[0px] lg:-ml-[0px] xl:-ml-[0px] 2xl:-ml-[280px] `}
+            } overflow-hidden absolute left-0 right-0 bg-[#e7e5e5] sm:mt-[150px] mt-80 sm:-ml-[0px] md:-ml-[0px] lg:-ml-[0px] xl:-ml-[0px] 2xl:-ml-[200px] `}
           >
             {/*Inicio cardilogia */}
             {openSection === "cardiología" && (
